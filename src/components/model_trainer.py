8646e112d4f916886901adadffd84fc590cda538
sankar480 <narayanansankar480@gmail.com>
2025-05-23 16:08:09 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,13 +43,52 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting":GradientBoostingRegressor(),
                 "Linear Regression" :LinearRegression(),
-                "K- Neighbors Regreesion ": KNeighborsRegressor(),
+                # "K- Neighbors Regreesion ": KNeighborsRegressor(),
                 "XGBRegression" : XGBRegressor(),
                 "CatBoosting Regression" :CatBoostRegressor(),
                 "AdaBoost Regression" : AdaBoostRegressor()
             }
 
-            model_report:dict= evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models= models)
+            params = {
+            "Decision Tree": {
+                'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                # 'splitter': ['best', 'random'],
+                # 'max_features': ['sqrt', 'log2'],
+            },
+            "Random Forest": {
+                # 'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                # 'max_features': ['sqrt', 'log2', None],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "Gradient Boosting": {
+                # 'loss': ['squared_error', 'huber', 'absolute_error', 'quantile'],
+                'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                # 'criterion': ['squared_error', 'friedman_mse'],
+                # 'max_features': ['auto', 'sqrt', 'log2'],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "Linear Regression": {},
+            "XGBRegression": {
+                'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "CatBoosting Regression": {
+                'depth': [6, 8, 10],
+                'learning_rate': [0.01, 0.05, 0.1],
+                'iterations': [30, 50, 100]
+            },
+            "AdaBoost Regression": {
+                'learning_rate': [0.1, 0.01, 0.5, 0.001],
+                # 'loss': ['linear', 'square', 'exponential'],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            # "K- Neighbors Regression": {}
+        }
+
+
+
+            model_report:dict= evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models= models,param =params)
 
             best_model_score = max(sorted(model_report.values()))
 
